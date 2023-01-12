c4ef0e2c0f6bf509f98441837bc27d0cf25d6c84
Petar Yordanov <Petar.Yordanov@adesso.bg>
2023-01-12 02:17:09 +0200

    first 3 JS lessons done

---
+++ cypress-web-automation-testing-from-zero-to-hero/JS Fundamentals/lessons/lesson1_VariablesAndDataTypes.js
@@ -0,0 +1,25 @@
+// 1.Hello World
+console.log("Hello World!");
+
+// Variables
+let firstName = "John";
+let lastName = "Smith";
+console.log(lastName);
+
+var age, dateOfBirth, sex;
+age = "5";
+sex = "Male";
+console.log(age);
+age = "6";
+console.log(age);
+
+// Constants
+const occupation = "Engineer";
+console.log(occupation);
+
+// Data Types
+let middleName = "David"; // string
+let ageOfBorther = 25; // number
+let isHeMarried = false; // boolean
+let yearsInMarriage = null; // null
+let numberOfCars = undefined; //
